e4d30d03e001c65ebf34c204aad2888c3dd31108
justamitsaha <justamitsaha@gmail.com>
2023-01-22 06:31:19 +0530

    CSS fix for carausal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'high-tech-bank-ui';
+export class AppComponent implements OnInit {
+
+  showSidebar: boolean = false;
+
+  ngOnInit(): void {
+    this.showSidebar = (window.location.href).includes("secure");
+  }
+
 }
