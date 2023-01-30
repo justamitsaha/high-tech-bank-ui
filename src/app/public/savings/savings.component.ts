518da5b88076d95d573c760c5218502334efdde7
justamitsaha <justamitsaha@gmail.com>
2023-01-30 17:18:44 +0530

    UI change static page

--- src/app/public/savings/savings.component.ts
+++ src/app/public/savings/savings.component.ts
@@ -11,6 +11,17 @@ export class SavingsComponent {
 
   showAccForm() {
     this.showForm = !this.showForm;
+    let selector = 'openAccount';
+    this.scrollToElement(`#${selector}`)
+  }
+
+  scrollToElement(element_ref: any, offset = 10) {
+    setTimeout(() => {
+      let is_selector = (typeof element_ref) == "string";
+      let element = (is_selector) ? document.querySelector(element_ref) : element_ref;
+      let scroll_extent = element.getBoundingClientRect().top + window.pageYOffset - offset;
+      window.scrollTo(0, scroll_extent);
+    }, 200);
   }
 
 }
